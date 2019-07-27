7c28feee35226d5ddc4d4bafdce6d20c4e7cc5f2
sofinms <sofinms@gmail.com>
2019-07-27 22:41:27 +0400

    corrected my code due to recent changes

--- lib/strategy/sofinms.rb
+++ lib/strategy/sofinms.rb
@@ -1,25 +1,29 @@
 module Strategy
 	class Sofinms < BaseStrategy
 		MOVES = %w(left right up down)
+		@road = []
 
-		def get_allowed_movements state
+		def initialize config
+			@confog = config
+			@road = []
+		end
+
+		def get_allowed_movements x, y, lines
 			allowed_movements = []
-			x = state["params"]["players"]["i"]["position"][0]
-			y = state["params"]["players"]["i"]["position"][1]
 			# right
-			if (x + 30) <= 900 and !state["params"]["players"]["i"]['lines'].include?([x+30, y]) 
+			if (x + 30) <= 900 and !lines.include?([x+30, y]) 
 				allowed_movements.push('right')
 			end
 			# left
-			if (x - 30) >= 0 and !state["params"]["players"]["i"]['lines'].include?([x-30, y]) 
+			if (x - 30) >= 0 and !lines.include?([x-30, y]) 
 				allowed_movements.push('left')
 			end
 			# up
-			if (y + 30) <= 900 and !state["params"]["players"]["i"]['lines'].include?([x, y+30]) 
+			if (y + 30) <= 900 and !lines.include?([x, y+30]) 
 				allowed_movements.push('up')
 			end
 			# down
-			if (y - 30) >= 0 and !state["params"]["players"]["i"]['lines'].include?([x, y-30]) 
+			if (y - 30) >= 0 and !lines.include?([x, y-30]) 
 				allowed_movements.push('down')
 			end
 
@@ -27,38 +31,40 @@ module Strategy
 		end
 
 		def get_next_position position, next_step
+			x = position.x
+			y = position.y
 			if next_step == 'right'
-				return [position[0] + 30, position[1]]
+				return {'x' => x + 30, 'y' => y}
 			end
 			if next_step == 'left'
-				return [position[0] -30, position[1]]
+				return {'x' => x -30, 'y' => y}
 			end
 			if next_step == 'up'
-				return [position[0], position[1]+30]
+				return {'x' => x + 30, 'y' => y+30}
 			end
 			if next_step == 'down'
-				return [position[0], position[1]-30]
+				return {'x' => x, 'y' => y-30}
 			end
 		end
 
 		def attach_territory state
-			if !state["params"]["players"]["i"]['lines'].empty?
-				x = state["params"]["players"]["i"]["position"][0]
-				y = state["params"]["players"]["i"]["position"][1]
+			if !state.me.lines.empty?
+				x = state.me.position.x
+				y = state.me.position.y
 				# right
-				if state["params"]["players"]["i"]['territory'].include?([x+30, y]) 
+				if state.me.territory.include?([x+30, y]) 
 					return 'right'
 				end
 				# left
-				if state["params"]["players"]["i"]['territory'].include?([x-30, y]) 
+				if state.me.territory.include?([x-30, y]) 
 					return 'left'
 				end
 				# up
-				if state["params"]["players"]["i"]['territory'].include?([x, y+30]) 
+				if state.me.territory.include?([x, y+30]) 
 					return 'up'
 				end
 				# down
-				if state["params"]["players"]["i"]['territory'].include?([x, y-30]) 
+				if state.me.territory.include?([x, y-30]) 
 					return 'down'
 				end
 			end
@@ -66,30 +72,52 @@ module Strategy
 			''
 		end
 
+		def check_for_return_to_territory state
+			if state.me.lines.length > 5
+				return true
+			end
+			return false
+		end
+
+		def create_road_to_territory
+
+		end
+
+		def next_move_from_road
+		end
+
 		def next_tick state
-			allowed_movements = get_allowed_movements state
+			allowed_movements = get_allowed_movements state.me.position.x, state.me.position.y, state.me.lines 
 
 			# Если можно на следующем шаге присоединить территорию, то делаем это
 			next_step = attach_territory state
 			# Если нет, то рандомим следующий шаг
 			if next_step.empty?
-				next_step = allowed_movements.sample
-				# Проверяем, что следующий шаг не приведёт нас в тупик
-				next_state = state
-				next_state["params"]["players"]["i"]["position"] = get_next_position next_state["params"]["players"]["i"]["position"], next_step
-				next_allowed_movements = get_allowed_movements state
+				# Если длина шлейфа слишком большая, то строим дорогу для возвращения по кратчайшему пути на территорию
+				if check_for_return_to_territory state
+					create_road_to_territory
+				end
 
-				if next_allowed_movements.empty?
-					allowed_movements.delete(next_step)
+				if !@road.empty?
+					next_step = next_move_from_road
+				else
 					next_step = allowed_movements.sample
+					# Проверяем, что следующий шаг не приведёт нас в тупик
+					next_position = get_next_position state.me.position, next_step
+					next_allowed_movements = get_allowed_movements next_position['x'], next_position['y'], state.me.lines
+
+					if next_allowed_movements.empty?
+						allowed_movements.delete(next_step)
+						next_step = allowed_movements.sample
+					end
 				end
 			end
 
 			file = File.open("/Users/sofinms/Sites/test_log", "w")
 			file.puts next_step
 			file.puts allowed_movements.to_json
-			file.puts state["params"]["players"]["i"]["position"].to_json
-			file.puts state["params"]["players"]["i"]['lines'].to_json
+			file.puts state.me.position.to_json
+			file.puts state.me.lines.to_json
 			file.close
 
 			next_step
