bdda04ffd0bba93d43a60c93868fbef2ab4969c1
sofinms <sofinms@gmail.com>
2019-07-27 14:25:47 +0400

    slightly improved its algorithm

--- lib/strategy/sofinms_algorithm.rb
+++ lib/strategy/sofinms_algorithm.rb
@@ -26,15 +26,73 @@ module Strategy
 			allowed_movements
 		end
 
+		def get_next_position position, next_step
+			if next_step == 'right'
+				return [position[0] + 30, position[1]]
+			end
+			if next_step == 'left'
+				return [position[0] -30, position[1]]
+			end
+			if next_step == 'up'
+				return [position[0], position[1]+30]
+			end
+			if next_step == 'down'
+				return [position[0], position[1]-30]
+			end
+		end
+
+		def attach_territory state
+			if !state["params"]["players"]["i"]['lines'].empty?
+				x = state["params"]["players"]["i"]["position"][0]
+				y = state["params"]["players"]["i"]["position"][1]
+				# right
+				if state["params"]["players"]["i"]['territory'].include?([x+30, y]) 
+					return 'right'
+				end
+				# left
+				if state["params"]["players"]["i"]['territory'].include?([x-30, y]) 
+					return 'left'
+				end
+				# up
+				if state["params"]["players"]["i"]['territory'].include?([x, y+30]) 
+					return 'up'
+				end
+				# down
+				if state["params"]["players"]["i"]['territory'].include?([x, y-30]) 
+					return 'down'
+				end
+			end
+
+			''
+		end
+
 		def next_tick config, state
-			state = game_config.get_current_state
 			allowed_movements = get_allowed_movements state
 
+			# Если можно на следующем шаге присоединить территорию, то делаем это
+			next_step = attach_territory state
+			# Если нет, то рандомим следующий шаг
+			if next_step.empty?
+				next_step = allowed_movements.sample
+				# Проверяем, что следующий шаг не приведёт нас в тупик
+				next_state = state
+				next_state["params"]["players"]["i"]["position"] = get_next_position next_state["params"]["players"]["i"]["position"], next_step
+				next_allowed_movements = get_allowed_movements state
+
+				if next_allowed_movements.empty?
+					allowed_movements.delete(next_step)
+					next_step = allowed_movements.sample
+				end
+			end
+
 			file = File.open("/Users/sofinms/Sites/test_log", "w")
+			file.puts next_step
 			file.puts allowed_movements.to_json
+			file.puts state["params"]["players"]["i"]["position"].to_json
+			file.puts state["params"]["players"]["i"]['lines'].to_json
 			file.close
 
-			allowed_movements.sample
+			next_step
 		end
 	end
 end
